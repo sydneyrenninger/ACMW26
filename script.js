1998f5598750401096a061520a0fa956852e2057
ririelle15 <namitapradeep.15@gmail.com>
2026-02-22 07:01:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -110,3 +110,57 @@ if (password && strengthBar && strengthText && toggleBtn) {
   activities.forEach(initActivity);
 })();
 
+/* Website acitivity - only on website.html */
+const questions = [
+  {
+    question: "Which is the real Amazon website?",
+    answers:[
+      {text: "https://www.amazon-shopping.net", correct: false}
+       {text: "https://www.amaz0n.com", correct: false}
+       {text: "http://amazon-deals.co", correct: false}
+       {text: "https://www.amazon.com", correct: true}
+    ]
+  }, 
+
+  {
+    question: "Which website option is the most credible for health information?",
+    answers:[
+      {text: "http://health-secrets-blog.com", correct: false}
+       {text: "https://www.cdc.gov", correct: true}
+       {text: "http://cdc-updates.net", correct: false}
+       {text: "https://cdc-notreal.org", correct: false}
+    ]
+  }, 
+
+  {
+    question: "Which website option is the most credible for space research?",
+    answers:[
+      {text: "https://www.nasa.gov", correct: true}
+       {text: "http://nasa-space-news.com", correct: false}
+       {text: "https://www.nasa-official.net", correct: false}
+       {text: "http://nasaupdates.info", correct: false}
+    ]
+  }, 
+
+   {
+    question: "Which website option is the most credible for news?",
+    answers:[
+      {text: "http://bbc-breaking-news.net", correct: false}
+       {text: "http://nasa-space-news.com", correct: true}
+       {text: "https://bbc-world-news.co", correct: false}
+       {text: "http://bbcarchive.internet", correct: false}
+    ]
+  }, 
+
+  {
+    question: "Which website option is the most credible for online banking?",
+    answers:[
+      {text: "http://chase-secure-login.net", correct: false}
+       {text: "https://www.chase.com", correct: true}
+       {text: "https://www.chasebank-login.co", correct: false}
+       {text: "http://secure-chase-info.org", correct: false}
+    ]
+  }, 
+  
+]
+
